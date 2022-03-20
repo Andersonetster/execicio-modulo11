d839e97a2eaf1d15c97eceb84042f87da00765b7
name <anderson.netster2012@gmail.com>
2022-03-20 15:26:43 -0300

    arquivo pre-cadastro finalizado.

---
+++ cypress/integration/pre-cadastro.spec.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress"/>
+//var faker = require('faker-br');
+//const faker = require('faker-br');
+var faker = require('faker-br');
+const Faker = require('faker-br/lib');
+
+
+describe('Funcionalidade Pré-Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('/minha-conta/')
+    });
+
+    it('Deve criar cadastro com sucesso', () => {
+        cy.get('#reg_email').type('anderson@santos5.com')
+        cy.get('#reg_password').type('teste@teste')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type('anderson')
+        cy.get('#account_last_name').type('santos')
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso')
+    });
+
+    it.only('Deve criar cadastro com sucesso cadastro faker', () => {
+
+        //cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('teste@teste')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.name.firstName())
+        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso')
+        
+    });
+
+
+    
+});
